86d9d37b334f7958fe5e7d541778e83c79a2f665
bennieb1 <97978644+bennieb1@users.noreply.github.com>
2023-11-28 13:22:07 -0500

    updated input system with less hardcoding

--- Engine/Engine/InputSystem.h
+++ Engine/Engine/InputSystem.h
@@ -17,7 +17,7 @@ public:
 		if (instance == nullptr)
 		{
 			instance = new InputSystem();
-			instance->Initialize();
+			
 		}
 		return *instance;
 	}
@@ -25,25 +25,36 @@ public:
 	~InputSystem();
 	void Initialize();
 	void update();
+
+	void triggerKeyEvent(SDL_Keycode key, bool pressed);
+
+	void triggerMouseEvent(Uint8 button, bool pressed);
+
 	bool isKeyPressed(SDL_Keycode key) const;
 	bool areKeysPressed(const std::vector<SDL_Keycode>& keys) const;
 	bool isMouseButtonPressed(Uint8 button) const;
-	void handleKeyPress(SDL_Keycode key);
-	void handleKeyRelease(SDL_Keycode key);
-	void handleMouseButtonPress(Uint8 button);
-	void handleMouseButtonRelease(Uint8 button);
 
-	void registerKeyEventHandler(SDL_Keycode key, std::function<void()> handler);
-	void registerMouseEventHandler(Uint8 button, std::function<void()> handler);
+	void registerQuitEventHandler(std::function<void()> handler);
+
+	void registerKeyEventHandler(SDL_Keycode key, bool onPress, std::function<void()> handler);
+	void registerMouseEventHandler(Uint8 button, bool onPress, std::function<void()> handler);
+
+
+	
 
 private:
-	std::map<SDL_Keycode, bool> keyStates;
-	std::map<Uint8, bool> mouseButtonStates;
+	bool keyStates[256];
+	bool mouseButtonStates[5];
 	InputSystem(const& inputSystem);
+
 	InputSystem operator = (const& InputSystem);
 	static InputSystem* instance;
-	std::map<SDL_Keycode, std::function<void()>> keyEventHandlers; // Caching function pointers for key events
-	std::map<Uint8, std::function<void()>> mouseEventHandlers; // Caching function pointers for mouse events
+	std::map<SDL_Keycode, std::function<void()>> keyPressHandlers;
+	std::map<SDL_Keycode, std::function<void()>> keyReleaseHandlers;
+	std::map<Uint8, std::function<void()>> mousePressHandlers;
+	std::map<Uint8, std::function<void()>> mouseReleaseHandlers;
+
+	std::function<void()> quitEventHandler;
 
 };
 
