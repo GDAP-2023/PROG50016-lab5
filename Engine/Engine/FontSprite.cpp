de8e4092fbbe911830bb575e9668c0d00febcacd
Beaudmat <97978688+Beaudmat@users.noreply.github.com>
2023-12-01 11:13:02 -0500

    Comments added to match the agreed documentation style
    Added comments to the headers and cpp files I worked on to match with the agreed upon style.

--- Engine/Engine/FontSprite.cpp
+++ Engine/Engine/FontSprite.cpp
@@ -1,3 +1,10 @@
+/*
+* @FontSprite
+*
+* Allows the user to display text onto the SDL Window. They can control the text content,
+* font used by providing a new asset to use, and the texts display color.
+*/
+
 #include "EngineCore.h"
 #include "FontSprite.h"
 #include "SDL_ttf.h";
@@ -37,26 +44,48 @@ void FontSprite::Render()
 	//SDL_RenderCopy(&RenderSystem::Instance().GetRenderer(), _output, NULL, &_fontRect);
 }
 
+/*
+* @SetText
+*
+* Takes a string for the display text and regenerates the font texture.
+*/
 void FontSprite::SetText(std::string text)
 {
 	_text = text;
 	RegenerateOutput();
 }
 
+/*
+* @SetFont
+*
+* Takes a TextureAsset for the new font and regenerates the font texture.
+*/
 void FontSprite::SetFont(TextureAsset* font)
 {
 	_font = font;
 	RegenerateOutput();
 }
 
+/*
+* @SetFontColor
+*
+* Takes RGB values along with an Alpha and regenerates the font texture.
+*/
 void FontSprite::SetFontColor(int r, int g, int b, int a)
 {
 	_fontColor.r = r;
 	_fontColor.g = g;
 	_fontColor.b = b;
 	_fontColor.a = a;
+	RegenerateOutput();
 }
 
+/*
+* @RegenerateOutput
+*
+* Used by other methods to regenerate the SDL_Texture after things like text, font or color
+* have changed.
+*/
 void FontSprite::RegenerateOutput()
 {
 	//SDL_Surface* textSurface = TTF_RenderText_Solid(TTF_OpenFont(_font.NAME, _font.SIZING), _text, _fontColor);
--- Engine/Engine/FontSprite.h
+++ Engine/Engine/FontSprite.h
@@ -2,6 +2,13 @@
 #ifndef _FONT_SPRITE_
 #define _FONT_SPRITE_
 
+/*
+* @FontSprite
+*
+* Allows the user to display text onto the SDL Window. They can control the text content,
+* font used by providing a new asset to use, and the texts display color. 
+*/
+
 #include <iostream>
 #include "Renderable.h"
 #include "SDL.h"
@@ -22,6 +29,12 @@ class FontSprite : public Renderable
 
 	SDL_Color _fontColor = { 255,255,255,255 };
 
+	/*
+	* @RegenerateOutput
+	* 
+	* Used by other methods to regenerate the SDL_Texture after things like text, font or color
+	* have changed.
+	*/
 	void RegenerateOutput();
 
 protected:
@@ -38,10 +51,25 @@ protected:
 
 public:
 
+	/*
+	* @SetText
+	* 
+	* Takes a string for the display text and regenerates the font texture.
+	*/
 	void SetText(std::string text);
 
+	/*
+	* @SetFont
+	* 
+	* Takes a TextureAsset for the new font and regenerates the font texture.
+	*/
 	void SetFont(TextureAsset* font);
 
+	/*
+	* @SetFontColor
+	* 
+	* Takes RGB values along with an Alpha and regenerates the font texture. 
+	*/
 	void SetFontColor(int r, int g, int b, int a);
 };
 
