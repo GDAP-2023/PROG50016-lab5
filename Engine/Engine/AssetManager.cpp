72730d7e911c7a54e6bf21f5787d512a169f0c47
Alvin Philips <alvinphilips257@gmail.com>
2023-12-01 15:34:22 -0500

    Added AssetManager to Engine

--- Engine/Engine/AssetManager.cpp
+++ Engine/Engine/AssetManager.cpp
@@ -1,55 +1,54 @@
 #include "EngineCore.h"
 #include "AssetManager.h"
-#include <iostream>
 
-AssetManager* AssetManager::instance = nullptr;
+#include <filesystem>
 
-void AssetManager::Initialize()
-{
-	std::cout << "AssetManager Initialize()" << std::endl;
+using std::filesystem::directory_iterator;
+using std::filesystem::recursive_directory_iterator;
+
+void AssetManager::Initialize() {
+	if (recursiveSearch)
+	{
+		for (const auto& entry: recursive_directory_iterator(assetDirectory))
+		{
+			std::cout << entry.path() << std::endl;
+		}
+	}
 }
 
-void AssetManager::AddAsset(Asset* newAsset)
-{
+void AssetManager::AddAsset(Asset* newAsset) {
 	assets.push_back(newAsset);
 }
 
-void AssetManager::RemoveAsset(Asset* asset)
-{
-	for (Asset* a : assets)
-	{
+void AssetManager::RemoveAsset(Asset* asset) {
+	for (Asset* a : assets) {
 		if (a == asset)
 			assets.remove(asset);
 	}
 }
 
-AssetManager::AssetManager()
-{
-	std::cout << "AssetManager Constructor" << std::endl;
+void AssetManager::Load(const std::string& config_file) {
+	std::ifstream inputStream(config_file.c_str());
+	std::string str((std::istreambuf_iterator<char>(inputStream)), std::istreambuf_iterator<char>());
+	json::JSON node = json::JSON::Load(str);
+
+	if (node.hasKey("AssetDirectory")) {
+		assetDirectory = node.at("AssetDirectory").ToString();
+	}
+
+	if (node.hasKey("RecursiveSearch")) {
+		recursiveSearch = node.at("RecursiveSearch").ToBool();
+	}
 }
 
-AssetManager::~AssetManager()
-{
-	std::cout << "AssetManager Destructor" << std::endl;
-}
-
-void AssetManager::LoadAsset(int AssetId)
-{
+void AssetManager::LoadAsset(int AssetId) {
 	//Check the map for loaded assets or do something
 }
 
-void AssetManager::Destroy()
-{
-	for (Asset* a : assets)
-	{
+void AssetManager::Destroy() {
+	for (Asset* a : assets) {
 		Asset* assetToDelete = a;
 		delete(assetToDelete);
 	}
 	assets.clear();
-
-	if (instance != nullptr)
-	{
-		delete instance;
-		instance = nullptr;
-	}
 }
