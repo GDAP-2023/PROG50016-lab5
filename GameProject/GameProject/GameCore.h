533a0bec128a0bbe2bb4e131629bcbef96dfedde
Alvin Philips <alvinphilips257@gmail.com>
2023-12-02 11:41:25 -0500

    Added common includes

--- GameProject/GameProject/GameCore.h
+++ GameProject/GameProject/GameCore.h
@@ -1,9 +1,15 @@
 #pragma once
+
 #ifndef _GAME_CORE_H_
+#define _GAME_CORE_H_
 
 #include "EngineCore.h"
+#include "RenderSystem.h"
+#include "Scene.h"
 #include "Engine.h"
-#include "Zombie.h"
-#include "Ghoul.h"
+#include "Entity.h"
+#include "InputSystem.h"
+#include "Component.h"
+#include "Player.h"
 
 #endif // !_GAME_CORE_H_
