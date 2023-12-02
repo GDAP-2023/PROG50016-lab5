e5312d235a72fefba54df22c95735bdba60c8610
bennieb1 <brandon961@live.ca>
2023-12-02 14:44:52 -0500

    made player movement more smooth

--- GameProject/GameProject/Player.cpp
+++ GameProject/GameProject/Player.cpp
@@ -9,29 +9,33 @@ void Player::Initialize()
 {
     Component::Initialize();
 }
-
 void Player::Update() {
     Vec2 dir = Vec2::Zero;
     const InputSystem& input = InputSystem::Instance();
 
+    // Handle horizontal movement
     if (input.isKeyPressed(SDLK_LEFT) || input.isKeyPressed(SDLK_a) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_LEFT)) {
-        dir.x -= 1;
+        dir.x = -1;
     }
-    if (input.isKeyPressed(SDLK_RIGHT) || input.isKeyPressed(SDLK_d) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_RIGHT)) {
-        dir.x += 1;
-    }
-    if (input.isKeyPressed(SDLK_UP) || input.isKeyPressed(SDLK_w) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_UP)) {
-        dir.y -= 1;
-    }
-    if (input.isKeyPressed(SDLK_DOWN) || input.isKeyPressed(SDLK_s) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_DOWN)) {
-        dir.y += 1;
+    else if (input.isKeyPressed(SDLK_RIGHT) || input.isKeyPressed(SDLK_d) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_RIGHT)) {
+        dir.x = 1;
     }
 
+    // Handle vertical movement
+    if (input.isKeyPressed(SDLK_UP) || input.isKeyPressed(SDLK_w) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_UP)) {
+        dir.y = -1;
+    }
+    else if (input.isKeyPressed(SDLK_DOWN) || input.isKeyPressed(SDLK_s) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_DOWN)) {
+        dir.y = 1;
+    }
+
+    // Handle gamepad analog stick input
     if (dir == Vec2::Zero) {
         dir.x = input.getGamepadAxisState(0, SDL_CONTROLLER_AXIS_LEFTX);
         dir.y = input.getGamepadAxisState(0, SDL_CONTROLLER_AXIS_LEFTY);
     }
 
+    // Normalize the direction vector if it's not zero
     if (dir != Vec2::Zero) {
         dir.Normalize();
 #ifdef DEBUG_PLAYER
@@ -39,9 +43,9 @@ void Player::Update() {
 #endif
     }
 
+    // Move the player
     ownerEntity->GetTransform().position += dir * (speed * Time::Instance().DeltaTime());
 }
-
 void Player::Load(json::JSON& node)
 {
     Component::Load(node);
